70ad3ed7966066cb747899acfa9006144f791849
Zyro <Aditymenon16@gmail.com>
2025-09-23 18:58:07 +0400

    Add files via upload

---
+++ dictionarys.py
@@ -0,0 +1,14 @@
+worldic_dict={'USA':200,
+              'india':100,
+              'Russia':400}
+#add
+worldic_dict['japan']=100
+print(worldic_dict)
+#remove
+del worldic_dict['india']
+print (worldic_dict)
+#update
+worldic_dict['india']=300
+print (worldic_dict)
+#lenght of dictionary
+print (len(worldic_dict))
