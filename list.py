5d63c675adfdf5678673e7e193c43119704d9369
Zyro <Aditymenon16@gmail.com>
2026-03-10 18:58:13 +0400

    Add files via upload

---
+++ list.py
@@ -0,0 +1,11 @@
+
+print("--- TO DO LIST ---" )
+print("1.View tasks ")
+print("2.add a task")
+print("3.remove a task")
+print("4.update the tasks")
+print("5.exit")
+user=input("please pick a option from 1-5 ")
+
+if user=="1":
+    print("hello")
