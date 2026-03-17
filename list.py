8636539bc9977a5af78d1e291dbdf40221241746
Zyro <Aditymenon16@gmail.com>
2026-03-17 18:56:34 +0400

    Add files via upload

--- list.py
+++ list.py
@@ -1,11 +1,20 @@
+tasks=[]
+while True:
+    print("--- TO DO LIST ---" )
+    print("1.View tasks ")
+    print("2.add a task")
+    print("3.remove a task")
+    print("4.update the tasks")
+    print("5.exit")
+    user=input("please pick a option from 1-5 ")
 
-print("--- TO DO LIST ---" )
-print("1.View tasks ")
-print("2.add a task")
-print("3.remove a task")
-print("4.update the tasks")
-print("5.exit")
-user=input("please pick a option from 1-5 ")
+    if user=="1":
+        for task in tasks:
+            print(task)
+    elif user=="2":
+        add=input("type the task you want to add ")
+        tasks.append(add)
 
-if user=="1":
-    print("hello")
+    elif user=="3":
+        delete=input("what do you want to remove? ")
+        tasks.remove(delete)
